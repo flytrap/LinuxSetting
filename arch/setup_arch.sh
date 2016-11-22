0136491d9b16dd4b8adf0d69c94d0b277f996f2a
flytrap <hiddenstat@gmail.com>
2016-11-22 22:40:15 +0800

    add i3 key to border

--- arch/setup_arch.sh
+++ arch/setup_arch.sh
@@ -1,8 +1,10 @@
 [ -d ~/.config/i3 ] || mkdir -p ~/.config/i3
 [ -d ~/.config/conky ] || mkdir -p ~/.config/conky
-cp -p ./i3_config ~/.config/i3/config
-cp ./startup.sh ~/.config/i3/startup.sh
-cp ./conky_start.sh ~/.config/i3/conky_start.sh
+p=`dirname $0`
+# echo $p
+cp -p $p/i3_config ~/.config/i3/config
+cp $p/startup.sh ~/.config/i3/startup.sh
+cp $p/conky_start.sh ~/.config/i3/conky_start.sh
 chmod u+x ~/.config/i3/startup.sh
 chmod u+x ~/.config/i3/conky_start.sh
-cp -p ./conky.conf ~/.config/conky/conky.conf
+cp -p $p/conky.conf ~/.config/conky/conky.conf
