40e2b81736a84f572ac487f457bef70c3aaec20f
flytrap <hiddenstat@gmail.com>
2018-06-02 21:26:54 +0800

    new: add index.html

--- coin/sh/flytrap_coin_minexmr.sh
+++ coin/sh/flytrap_coin_minexmr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-workon coin
+
 cd 
 cd github/coinhive-stratum-mining-proxy
-python coinhive-stratum-mining-proxy.py sg.minexmr.com 3333 --websocket_port 8894
+~/.virtualenvs/coin/bin/python coinhive-stratum-mining-proxy.py sg.minexmr.com 3333 --websocket_port 8894
--- coin/sh/flytrap_coin_supportxmr.sh
+++ coin/sh/flytrap_coin_supportxmr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-workon coin
+
 cd 
 cd github/coinhive-stratum-mining-proxy
-python coinhive-stratum-mining-proxy.py pool.supportxmr.com 5555 --websocket_port 8892
+~/.virtualenvs/coin/bin/python coinhive-stratum-mining-proxy.py pool.supportxmr.com 5555 --websocket_port 8892
