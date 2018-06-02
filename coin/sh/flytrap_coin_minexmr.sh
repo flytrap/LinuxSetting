8046ebf784a504798bdf4ffc3a78b7a2382f72e3
flytrap <hiddenstat@gmail.com>
2018-06-02 20:20:00 +0800

    chg: add linsten port

--- coin/sh/flytrap_coin_minexmr.sh
+++ coin/sh/flytrap_coin_minexmr.sh
@@ -2,4 +2,4 @@
 workon coin
 cd 
 cd github/coinhive-stratum-mining-proxy
-python coinhive-stratum-mining-proxy.py sg.minexmr.com 3333
+python coinhive-stratum-mining-proxy.py sg.minexmr.com 3333 --websocket_port 8894
--- coin/sh/flytrap_coin_supportxmr.sh
+++ coin/sh/flytrap_coin_supportxmr.sh
@@ -2,4 +2,4 @@
 workon coin
 cd 
 cd github/coinhive-stratum-mining-proxy
-python coinhive-stratum-mining-proxy.py pool.supportxmr.com 5555
+python coinhive-stratum-mining-proxy.py pool.supportxmr.com 5555 --websocket_port 8892
