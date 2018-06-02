3e8cd9cfa4c40edd1ba679f504f20dc165fe1f55
flytrap <hiddenstat@gmail.com>
2018-06-02 20:14:01 +0800

    new: add coin ssh

---
+++ coin/sh/flytrap_coin_minexmr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+workon coin
+cd 
+cd github/coinhive-stratum-mining-proxy
+python coinhive-stratum-mining-proxy.py sg.minexmr.com 3333
---
+++ coin/sh/flytrap_coin_supportxmr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+workon coin
+cd 
+cd github/coinhive-stratum-mining-proxy
+python coinhive-stratum-mining-proxy.py pool.supportxmr.com 5555
